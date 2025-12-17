9ec55c0378b54a4f9efb7a6c5a03731aac897be2
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-12-17 15:24:14 +0530

    [Nikhil Chahar] Add a leetcode solution of minimum size subarray

---
+++ leet-code-codebase/minimum_size_subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int sum = 0, min = Integer.MAX_VALUE, l = 0;
+        for(int r=0;r<nums.length;r++){
+            sum += nums[r];
+            while(sum>=target){
+                min = Math.min(min, r-l+1);
+                sum -= nums[l++];
+            }
+        }
+        return (min == Integer.MAX_VALUE) ? 0:min;
+    }
+}
