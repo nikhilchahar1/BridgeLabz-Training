aa823ccdfe45978e89fc9c2784bdd3baa872002f
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-12-18 11:05:56 +0530

    Added a java code for finding Sam's average marks

---
+++ gcr-codebase/programming-elements/Samsmark.java
@@ -0,0 +1,10 @@
+public class Samsmark {
+
+	public static void main(String[] args) {
+		double phyMarks = 95; //Sam's Physics marks
+		double mathMarks = 94; // maths marks
+		double chemMarks = 96; // chemistry marks
+		System.out.print("Sam's average marks in PCM is " +((phyMarks+mathMarks+chemMarks)/3)); // print average of three marks
+	}
+
+}
