8bf04b995f5e0c4afb14ab0f8503344ee26918e2
Nikhil Chahar <nikhilchahar592@gmail.com>
2026-01-13 15:30:12 +0530

    [Nikhil Chahar] Add .Added regex and Junit practice programs

---
+++ gcr-codebase/regex-junit/CalculatorTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
+
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(4, 2));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(4, 0));
+    }
+}
+
